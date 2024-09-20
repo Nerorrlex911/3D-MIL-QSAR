656b7fcbdca5f149c555b935eac570e2a4fc93ae
Nerorrlex911 <nerorrlex911@outlook.com>
2024-09-20 19:40:16 +0800

    numpy

--- run.sh
+++ run.sh
@@ -1,12 +1,13 @@
-pip install torch==1.12.1+cu113 torchvision==0.13.1+cu113 torchaudio==0.12.1 --extra-index-url https://download.pytorch.org/whl/cu113
-pip install joblib -i https://pypi.org/simple
-pip install numpy -i https://pypi.org/simple
-pip install pandas -i https://pypi.org/simple
-pip install scikit-learn -i https://pypi.org/simple
-pip install rdkit==2023.9.4 -i https://pypi.org/simple
-pip install networkx -i https://pypi.org/simple
-pip install matplotlib -i https://pypi.org/simple
-pip install pmapper -i https://pypi.org/simple
-pip install torch-optimizer -i https://pypi.org/simple
-pip install seaborn -i https://pypi.org/simple
-conda install openbabel -c conda-forge
+# pip install torch==1.12.1+cu113 torchvision==0.13.1+cu113 torchaudio==0.12.1 --extra-index-url https://download.pytorch.org/whl/cu113
+# pip install joblib -i https://pypi.org/simple
+# pip install numpy -i https://pypi.org/simple
+# pip install pandas -i https://pypi.org/simple
+# pip install scikit-learn -i https://pypi.org/simple
+# pip install rdkit==2023.9.4 -i https://pypi.org/simple
+# pip install networkx -i https://pypi.org/simple
+# pip install matplotlib -i https://pypi.org/simple
+# pip install pmapper -i https://pypi.org/simple
+# pip install torch-optimizer -i https://pypi.org/simple
+# pip install seaborn -i https://pypi.org/simple
+# conda install openbabel -c conda-forge
+pip install numpy==1.24.1
